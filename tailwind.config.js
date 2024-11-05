95a3aba77ec58564c6b1d1bda3987c0389f20ae2
Ekate Razuyeva <25ky-ky08@mail.ru>
2024-11-05 18:38:03 +0300

    contact page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,34 +7,36 @@ module.exports = {
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
-    	extend: {
-    		colors: {
-    			primary: '#1c1c22',
-    			accent: {
-    				DEFAULT: '#F57C00',
-    				hover: '#E65100'
-    			}
-    		},
-    		borderRadius: {
-    			lg: 'var(--radius)',
-    			md: 'calc(var(--radius) - 2px)',
-    			sm: 'calc(var(--radius) - 4px)'
-    		}
-    	},
-    	container: {
-    		center: 'true',
-    		padding: '15px'
-    	},
-    	screens: {
-    		sm: '640px',
-    		md: '768px',
-    		lg: '960px',
-    		xl: '1200px'
-    	},
-    	fontFamily: {
-    		mono: ['GetBrains Mono', 'monospace'],
-    		primary: 'var(--font-jetbrainsMono)'
-    	}
+        extend: {
+            colors: {
+                primary: '#1c1c22',
+                accent: {
+                    DEFAULT: '#F57C00',
+                    hover: '#E65100'
+                }
+            },
+            borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)'
+            },
+        },
+        container: {
+            center: 'true',
+            padding: '15px'
+        },
+        screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '960px',
+            xl: '1200px'
+        },
+        fontFamily: {
+            mono: ['GetBrains Mono', 'monospace'],
+            primary: 'var(--font-jetbrainsMono)'
+        },
+
+
     },
     plugins: [require("tailwindcss-animate")],
 };
