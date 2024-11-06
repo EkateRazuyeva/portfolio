9faeac55624b212376cc68f6cf6a8de30e3df7d3
Ekate Razuyeva <25ky-ky08@mail.ru>
2024-11-06 23:42:45 +0300

    add links

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,13 +1,12 @@
 import Link from "next/link";
 
-import {FaGithub, FaLinkedin, FaTelegram, FaCode} from "react-icons/fa";
+import {FaGithub, FaLinkedin, FaTelegram} from "react-icons/fa";
 
 
 const socials = [
-    {icon: <FaGithub className="text-3xl"/>, path: ""},
-    {icon: <FaLinkedin className="text-3xl"/>, path: ""},
-    {icon: <FaTelegram className="text-3xl"/>, path: ""},
-    {icon: <FaCode className="text-3xl"/>, path: ""},
+    {icon: <FaGithub className="text-3xl"/>, path: "https://github.com/EkateRazuyeva"},
+    {icon: <FaLinkedin className="text-3xl"/>, path: "https://www.linkedin.com/in/ekaterina-razuyeva-978915318/"},
+    {icon: <FaTelegram className="text-3xl"/>, path: "https://t.me/ekate_25"},
 ]
 
 const Social = ({containerStyles, iconStyles}) => {
@@ -15,7 +14,7 @@ const Social = ({containerStyles, iconStyles}) => {
         <div className={containerStyles}> {
             socials.map((item, index) => {
                 return (
-                    <Link key={index} href={item.path} className={iconStyles}>
+                    <Link target="_blank" rel="noopener noreferrer" key={index} href={item.path} className={iconStyles}>
                         {item.icon}
                     </Link>
                 )
